560c745d21e612d3417950176b5b753c2fb8eb3a
KhYaroslav <icpr.johni@gmail.com>
2023-04-29 20:57:08 +0300

    start media request

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
 import './style.css'
 import logo from '../../assets/svg/logo-icon.svg';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import close from '../../assets/svg/close-icon.svg';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
 
-  const Links = ['Home', 'Market', 'Discover', 'Commonity', 'Wallet']
+  const Links = ['Home', 'Market', 'Discover', 'Commonty', 'Wallet'];
+
+  const openNavHandler = () => {
+    setOpen((prev) => !prev)
+  }
 
   return (
     <div className="header">
@@ -15,21 +22,28 @@ export default function Header() {
             <p className='header__logo-title'>Nifter</p>
           </Link>
         </div>
-        <nav className='header__list'>
+        <nav className={`header__list ${open ? 'header__list-active' : ''}`}>
           <ul className='header__list-items'>
             {Links.map((link) =>
-              <Link to='#!' className='header__list-item'>
-                {link}
-              </Link>)}
+              <li className='header__list-item'>
+                <Link to='#!' className='header__list-item-link'>
+                  {link}
+                </Link>
+              </li>
+              )}
           </ul>
+        <div className='header__list-close' onClick={openNavHandler}>
+          <img src={close} alt="close" />
+        </div>
         </nav>
-        <div className='header__explors'>
-          <div className='header__expolors-wrapper'>
-            <div className='header__explors-items'>
-              <input placeholder='Search' className='header__expolors-field' />
-              <Link to='#!' className='header__expolors-link'>Explor</Link>
-            </div>
-          </div>
+        <div className='header__burger burger' onClick={openNavHandler}>
+            <span className='burger__line burger__line-first'></span>
+            <span className='burger__line burger__line-second'></span>
+            <span className='burger__line burger__line-third'></span>
+        </div>
+        <div className='header__explors-items'>
+          <input placeholder='Search' className='header__expolors-field' />
+          <Link to='#!' className='header__expolors-link'>Explor</Link>
         </div>
       </div>
     </div>
